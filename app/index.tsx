b5af61510a1e7aa6b865bf3c71de20e9c3eff543
Fernando Herrera <fernando.herrera85@gmail.com>
2024-08-27 09:52:18 -0400

    chore: Update dependencies and configuration files

---
+++ app/index.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native';
+const App = () => {
+  return (
+    <View className="mt-10">
+      <Text className="text-3xl font-bold text-blue-500">Hola Mundo</Text>
+    </View>
+  );
+};
+export default App;
