8e586be12ed90c6868b5d997116d9e1750ff5aeb
Fernando Herrera <fernando.herrera85@gmail.com>
2024-08-27 11:27:17 -0400

    chore: Update StackLayout component with new screenOptions

--- app/(stack)/_layout.tsx
+++ app/(stack)/_layout.tsx
@@ -1,7 +1,15 @@
 import { Stack } from 'expo-router';
 const StackLayout = () => {
   return (
-    <Stack>
+    <Stack
+      screenOptions={{
+        // headerShown: false,
+        headerShadowVisible: false,
+        contentStyle: {
+          backgroundColor: 'white',
+        },
+      }}
+    >
       <Stack.Screen
         name="home/index"
         options={{
